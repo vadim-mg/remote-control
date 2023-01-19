f3c01fbf2a6798a643b3137aa22b08f5c791ead6
vadim-mg <vadikm1981@yandex.ru>
2023-01-19 20:41:17 +0500

    feat: implement run WebSocketServer

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import { httpServer } from "./src/http_server/index.js";
-// import { mouse } from "@nut-tree/nut-js";
+import { WebSocketServer } from "ws";
+import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 }); 
+
+wss.on('connection', ws => {
+  ws.on('message', function message(data) {
+    // console.log( data);
+    console.log( data.toString());
+  });
+  console.log('Connection accepted!')
+  ws.send('something');
+})
