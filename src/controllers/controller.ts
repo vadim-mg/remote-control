d2a48fb22e2ce29341e812ce4a25eae94fb2b6af
vadim-mg <vadikm1981@yandex.ru>
2023-01-20 20:19:58 +0500

    feat: implement draw figures

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,18 +1,20 @@
-import { mouseCommand } from "./mouseController.js"
+import { mouseCommand, drawCommand } from "./mouseController.js"
 
 export const controller = async (message: string): Promise<string | undefined> => {
 
   console.log(message)
 
-  
-  const parsedMessage = message.split(/\ |\_/)
 
-  console.log(parsedMessage)
-  switch (parsedMessage[0]) {
+  const [command, type, ...args] = message.split(/\ |\_/)
+
+  // console.log(command)
+  // console.log(type)
+  console.log(args)
+  switch (command) {
     case 'mouse':
-      return (await mouseCommand(parsedMessage[1], +parsedMessage[2]))
+      return (await mouseCommand(type, args))
     case 'draw':
-      return message;
+      return (await drawCommand(type, args))
     default:
       return undefined
   }
--- src/controllers/mouseController.ts
+++ src/controllers/mouseController.ts
@@ -1,4 +1,4 @@
-import { mouse, left, up, right, down, Point } from "@nut-tree/nut-js";
+import { mouse, left, up, right, down, Point, Button, straightTo } from "@nut-tree/nut-js";
 
 const DIRECTIONS: { [key: string]: Function } = {
   up,
@@ -7,12 +7,63 @@ const DIRECTIONS: { [key: string]: Function } = {
   right,
 }
 
-const pointToString = (point: Point) => `x:${point.x},y:${point.y}`
+// const pointToString = (point: Point) => `mouse_position ${point.x},${point.y}`
 
-export const mouseCommand = async (type: string, arg: number) => {
-  if (type === 'position') {
-    return pointToString(await mouse.getPosition())
+const draw_rectangle = async (width: number, length: number) => {
+  await mouse.move(right(width))
+  await mouse.move(down(length))
+  await mouse.move(left(width))
+  await mouse.move(up(length))
+}
+
+const draw_circle = async (width: number) => {
+  const r = width / 2
+  const countPoints = width * 2
+
+  const startPoint: Point = await mouse.getPosition().then(val => val)
+  let point = new Point(startPoint.x, startPoint.y)
+
+  let i = 0
+  while (i <= countPoints) {
+    let x = i < width ? i : countPoints - i
+    let y = (i < width ? 1 : -1) * Math.sqrt(Math.pow(r, 2) - Math.pow(r - x, 2))
+    point.x = Math.round(startPoint.x + x)
+    point.y = Math.round(startPoint.y + y)
+    await mouse.move(straightTo(point))
+
+    if (i === width || i === countPoints) {
+      point.y = startPoint.y //fit lasts point for semicircle
+      await mouse.move(straightTo(point))
+    }
+    i++
   }
-  await mouse.move(DIRECTIONS[type](arg))
+}
+
+const mouseCommand = async (type: string, args: string[]) => {
+  if (type === 'position') {
+    return await mouse.getPosition().then((point: Point) => `mouse_position ${point.x},${point.y}`)
+  }
+  await mouse.move(DIRECTIONS[type](+args[0]))
   return type
 }
+
+
+const drawCommand = async (type: string, args: string[]) => {
+  await mouse.pressButton(Button.LEFT)
+
+  if (type === 'rectangle') {
+    await draw_rectangle(+args[0], +args[1])
+  }
+  if (type === 'square') {
+    await draw_rectangle(+args[0], +args[0])
+  }
+  if (type === 'circle') {
+    await draw_circle(+args[0])
+  }
+
+  await mouse.releaseButton(Button.LEFT)
+
+  return type
+}
+
+export { mouseCommand, drawCommand }
