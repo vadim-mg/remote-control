589c7514768108ff2a63165fac37cb7c7459b54b
vadim-mg <vadikm1981@yandex.ru>
2023-01-23 13:31:00 +0500

    refactor:  printFunction

--- src/commands/printCommands.ts
+++ src/commands/printCommands.ts
@@ -1,31 +1,43 @@
-import { mouse, Region, screen } from "@nut-tree/nut-js";
+import { mouse, Region, screen, Point } from "@nut-tree/nut-js";
 import Jimp from 'jimp';
+
+const PRINT_SCREEN_AREA_SIZE = 200
+
+
+const getScreenArea = (
+  point: Point,
+  screenWidth: number,
+  screenHight: number,
+  areaSize: number
+): Region => new Region(
+  Math.min(Math.max(point.x - Math.round(areaSize / 2), 0), screenWidth - areaSize),
+  Math.min(Math.max(point.y - Math.round(areaSize / 2), 0), screenHight - areaSize),
+  areaSize,
+  areaSize
+)
 
 
 const getPartScreen = async (areaSize: number) => {
-  const point = await mouse.getPosition()
-  const maxX = await screen.width()
-  const maxY = await screen.height()
-
-  const region = new Region(
-    Math.min(Math.max(point.x - Math.round(areaSize / 2), 0), maxX - areaSize),
-    Math.min(Math.max(point.y - Math.round(areaSize / 2), 0), maxY - areaSize),
-    areaSize,
-    areaSize
-  )
-
-  const grabbedImage = await (await screen.grabRegion(region)).toRGB()
-
-  return await new Jimp(grabbedImage)
-    .getBase64Async('image/png')
-    .then(val => val.replace('data:image/png;base64,', ''))
-
+  return Promise.all([
+    mouse.getPosition(),
+    screen.width(),
+    screen.height()
+  ])
+    .then((values) => getScreenArea(...values, areaSize))
+    .then(region => screen.grabRegion(region))
+    .then(img => img.toRGB())
+    .then(img => new Jimp(img).getBufferAsync(Jimp.MIME_PNG))
+    .then(buf => buf.toString('base64'))
+    .catch(err => {
+      console.error(err)
+      return ''
+    })
 }
 
 
 const printCommand = async (type: string, args?: string[]) => {
   if (type === "scrn") {
-    return await getPartScreen(200)
+    return await getPartScreen(PRINT_SCREEN_AREA_SIZE)
   }
   return ''
 }
